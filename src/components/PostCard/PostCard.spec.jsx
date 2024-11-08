3db50c9e1ef8025bc7e3d6ed1fd484331b75c2a2
Fernando <fernandocarvalho.sena@gmail.com>
2024-11-08 10:23:59 -0300

    test: :white_check_mark: Adicionado testes

---
+++ src/components/PostCard/PostCard.spec.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { PostCard } from '.'
+import { PostCardPopsMock } from "./mock";
+
+const props = PostCardPopsMock;
+
+describe('<PostCard />', () => {
+    it('should render PostCard correctly', () => {
+        render(<PostCard {...props} />);
+        
+        expect.assertions(3);
+
+        expect(screen.getByAltText('title 1'))
+        .toHaveAttribute('src', 'img/img.png');
+        expect(screen.getByRole('heading', { name: 'title 1'})).toBeInTheDocument();
+        expect(screen.getByText('body 1')).toBeInTheDocument();
+    });
+
+    it('should match snapshot', () => {
+        const { container } = render(<PostCard {...props} />);
+        expect(container.firstChild).toMatchSnapshot();
+    });
+})
---
+++ src/components/PostCard/mock.js
@@ -0,0 +1,5 @@
+export const PostCardPopsMock = {
+    title: 'title 1',
+    body: 'body 1',
+    cover: 'img/img.png'
+}
