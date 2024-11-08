cffcdbe8e3feecaa5d11f295a22e3e3a8cb5465b
Fernando <fernandocarvalho.sena@gmail.com>
2024-11-08 12:37:17 -0300

    refactor: Alterado textInput

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,6 +1,6 @@
 import P from 'prop-types';
 import './styles.css';
-export const TextInput = ({ searchValue = '', handleChange }) => {
+export const TextInput = ({ searchValue, handleChange }) => {
   return (
     <input
       className="text-input"
